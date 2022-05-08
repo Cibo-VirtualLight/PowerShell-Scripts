21fce11423a8b9868f370fc3e12afc49d7221d83
Cibo-VirtualLight <49093962+Cibo-VirtualLight@users.noreply.github.com>
2022-05-08 12:31:26 +0800

    Update FileNameManipulation.ps1

--- FileNameManipulation.ps1
+++ FileNameManipulation.ps1
@@ -8,3 +8,14 @@ $partStr = $_.Name.Substring($_.Name.Length -6)
 $partStr = $partStr.Substring(0,2)
 Rename-Item -path $_.FullName -newname ($partStr + $_.Name)
 Write-Host $_.Name}
+
+#Rename your video files downloaded from YOUTUBE, find the serial number in the end and take it to the front
+#Previous -> Grocery shopping in German _ Super Easy German (33).mp4
+#Now -> 33Grocery shopping in German _ Super Easy German (33).mp4
+gci "F:\LearnGerman\EasyGermanVideoA1-A2" | ForEach-Object{ 
+write-host $_.Name
+$seriesNumber = $_.Name.Substring($_.Name.Length -10).Substring(0,6)
+$seriesNumber = $seriesNumber -replace "[^0-9]" , ''
+Rename-Item -path $_.FullName -newname ($seriesNumber + $_.Name)
+write-host $seriesNumber
+}
